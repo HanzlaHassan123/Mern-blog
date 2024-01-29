7962628a7147ab8b65008cd66792bc25d64fa10b
hp <tararhanzla783@gmail.co,m>
2024-01-29 12:29:53 +0500

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import { About } from "./pages/About"
+import SignIn from "./pages/SignIn"
+import { SignUp } from "./pages/SignUp"
+import { DashBoard } from "./pages/DashBoard"
+import { Projects } from "./pages/Projects"
 
 export default function App () {
   return (
-    <div>App</div>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/Sign-in" element={<SignIn/>}/>
+      <Route path="/Sign-up" element={<SignUp/>}/>
+      <Route path="/dashboard" element={<DashBoard/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
